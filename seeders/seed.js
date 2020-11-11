1483f6f2e51f16f6f091cb6098025b22051c39e1
taliavazquez <vazqueztalia@outlook.com>
2020-11-11 15:50:45 -0800

    removed uneeded comment and updated path in html

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,11 +1,6 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-// mongoose.connect("mongodb://localhost/workout", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
   {
